88c05da9216e4c2931b4e7c730a133658fa5b445
Jin Tao <jin.tao@silo.ai>
2023-08-03 13:08:16 +0300

    update iataCode

--- flight-deals-start/flight_search.py
+++ flight-deals-start/flight_search.py
@@ -1,8 +1,25 @@
+import requests
+
+LOCATION_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
+LOCATION_API_KEY = "DbITEzJi1mV-aVk9f90qDGX4NsZfVvwR"
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
 
     def get_destination_code(self, city_name):
-        code = "TESTING"
-        return code
 
+        header = {
+            "apikey": LOCATION_API_KEY
+        }
+
+        parameter = {
+            "term": city_name,
+            "location_types": "city"
+        }
+
+        response = requests.get(url=LOCATION_ENDPOINT, headers=header, params=parameter)
+        response.raise_for_status()
+        return response.json()["locations"][0]["code"]
+
+        # code = "TESTING"
+        # return code
