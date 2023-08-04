5c346538b58a6f72b04f0574eb2db015348d85b7
Jin Tao <jin.tao@silo.ai>
2023-08-04 18:27:05 +0300

    little revision on format

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -3,8 +3,9 @@ from twilio.rest import Client
 ACCOUNT_SID = "AC4359b26b93c3ea67351a7c33d0184a81"
 AUTH_TOKEN = "fb4819bf7d25d3eb31274b0fc38d4a9f"
 
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
+    # This class is responsible for sending notifications with the deal flight details.
 
     def sent_notification(self, price, departure_city_name, departure_airport_iata, arrival_city_name,
                           arrival_airport_iata, outbound_data, inbound_date):
