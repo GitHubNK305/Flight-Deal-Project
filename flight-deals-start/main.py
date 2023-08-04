11f5936f53a8a9604a3c2d4d2ad282dbf714a188
Jin Tao <jin.tao@silo.ai>
2023-08-04 11:11:31 +0300

    Find the lowest price and send notification using SMS

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,16 +1,18 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
 from pprint import pprint
 from datetime import datetime, timedelta
+from notification_manager import NotificationManager
 
 ORIGIN_CITY_CODE = "HEL"
 TOMORROW = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y")
 SIX_MONTHS_FROM_TODAY = (datetime.now() + timedelta(days=180)).strftime("%d/%m/%Y")
 
-# Create DataManager class and FlightSearch Class
+# Create DataManager class and FlightSearch Class6
 data_manage = DataManager()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 sheet_data = data_manage.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
@@ -19,5 +21,12 @@ if sheet_data[0]["iataCode"] == "":
     data_manage.update_destination_code()
 
 for city in data_manage.destination_data:
-    flight_search.flight_check(origin_city_code=ORIGIN_CITY_CODE, destination_city_code=city["iataCode"], from_time=TOMORROW, to_time=SIX_MONTHS_FROM_TODAY)
+    flight_data = flight_search.flight_check(origin_city_code=ORIGIN_CITY_CODE, destination_city_code=city["iataCode"],
+                                             from_time=TOMORROW, to_time=SIX_MONTHS_FROM_TODAY)
+    if flight_data.price < city["lowestPrice"]:
+        print("You can buy your ticket and plan your trip.")
+        notification_manager.sent_notification(price=flight_data.price, departure_city_name=flight_data.origin_city,
+                                               departure_airport_iata=flight_data.origin_airport, arrival_city_name=flight_data.destination_city,
+                                               arrival_airport_iata=flight_data.destination_airport, outbound_data=flight_data.out_date,
+                                               inbound_date=flight_data.return_date)
 # pprint(data_manage.destination_data)
--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,22 @@
+from twilio.rest import Client
+
+ACCOUNT_SID = "AC4359b26b93c3ea67351a7c33d0184a81"
+AUTH_TOKEN = "fb4819bf7d25d3eb31274b0fc38d4a9f"
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def sent_notification(self, price, departure_city_name, departure_airport_iata, arrival_city_name,
+                          arrival_airport_iata, outbound_data, inbound_date):
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+        message = client.messages.create(
+            body=f"Low price alert! Only €{price} to flight from"
+                 f"{departure_city_name}-{departure_airport_iata}"
+                 f" to {arrival_city_name}-{arrival_airport_iata},"
+                 f"from {outbound_data} to {inbound_date}",
+            from_='+19382014061',
+            to='+358509184234'
+        )
+
+        print(message.status)
